3820c69d618a3fe5775db1fe2f9f840bba34b6ce
aakash <aakashyadav112000@gmail.com>
2024-08-09 15:44:52 +0530

    adding few project in vue

---
+++ project3-todo/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodoListVue from "./components/TodoList.vue";
+
+</script>
+
+<template>
+<div>
+ <TodoListVue/>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ project4- RandomQuoteGenerator/QouterGenerator/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Generator from "./components/Generator.vue"
+import calculator from "./components/calculator.vue"
+import accordian from "./components/Accordian.vue"
+
+</script>
+
+<template>
+<!-- <Generator/> -->
+<accordian/>
+<!-- <calculator/> -->
+</template>
+
+<style scoped>
+
+</style>
